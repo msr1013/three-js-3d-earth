0464a41a96b7faf994862d85998c7166ed1d0454
Manish Rawal <manish.rawal@convergetree.com>
2022-03-23 14:36:34 +0545

    map texture into sphere

--- 3d earth/main.js
+++ 3d earth/main.js
@@ -14,11 +14,14 @@ const camera = new THREE.
   renderer.setSize(innerWidth,innerHeight)
   document.body.appendChild(renderer.domElement)
 
+
+
   //create a sphere
   const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5,50,50),
     new THREE.MeshBasicMaterial({
-      color:0xff0000
+      // color:0xff0000
+      map:new THREE.TextureLoader().load('/img/globe.jpg')
     })
   )
 
