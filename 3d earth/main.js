c097acb40c1e37c875a750320a6045c6c07ee9ee
Manish Rawal <manish.rawal@convergetree.com>
2022-03-25 15:11:05 +0545

    add shade of blue/red to earth texture

--- 3d earth/main.js
+++ 3d earth/main.js
@@ -1,4 +1,7 @@
-import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
+import * as THREE from 'three'
+import vertexShader from './shaders/vertex.glsl'
+import  fragmentShader from './shaders/fragment.glsl'
+
 
 const scene = new THREE.Scene()
 const camera = new THREE.
@@ -9,9 +12,12 @@ const camera = new THREE.
     1000
   )
 
-  const renderer = new THREE.WebGLRenderer(
+  const renderer = new THREE.WebGLRenderer({
+    antialias:true
+  }
   )
   renderer.setSize(innerWidth,innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
   document.body.appendChild(renderer.domElement)
 
 
@@ -19,14 +25,33 @@ const camera = new THREE.
   //create a sphere
   const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5,50,50),
-    new THREE.MeshBasicMaterial({
-      // color:0xff0000
-      map:new THREE.TextureLoader().load('/img/globe.jpg')
+    new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+      uniforms:{
+        globeTexture:{
+          value: new THREE.TextureLoader().load('./img/globe.jpeg')
+        }
+      }
     })
   )
 
    scene.add(sphere)
-   camera.position.z = 10
+
+  // create atmosphere
+  const atmosphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5,50,50),
+    new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+    })
+  )
+
+  atmosphere.scale.set(1.1,1.1,1.1)
+
+   scene.add(atmosphere)
+
+   camera.position.z = 15
 
   function animate() {
     requestAnimationFrame(animate)
