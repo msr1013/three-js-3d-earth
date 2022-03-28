52519f7e2721e8347edc8a09794ba65529c83197
Manish Rawal <manish.rawal@convergetree.com>
2022-03-28 16:00:36 +0545

    add background stars to scene

--- 3d earth/main.js
+++ 3d earth/main.js
@@ -5,6 +5,7 @@ import  fragmentShader from './shaders/fragment.glsl'
 
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
 import  atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+import { Float32BufferAttribute } from 'three'
 
 
 const scene = new THREE.Scene()
@@ -59,6 +60,25 @@ const camera = new THREE.
    const group = new THREE.Group()
    group.add(sphere)
    scene.add(group)
+
+   const starGeometry = new THREE.BufferGeometry()
+   const starMaterial = new THREE.PointsMaterial({
+     color:0xffffff
+   })
+
+   const starVertices = []
+   for (let i=0;i<10000;i++) {
+     const x = (Math.random() - 0.5) * 2000
+     const y = (Math.random() -0.5) * 2000
+     const z = -Math.random() * 3000
+     starVertices.push(x,y,z)
+   }
+
+
+   starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices,3))
+
+   const stars = new THREE.Points(starGeometry,starMaterial)
+   scene.add(stars)
 
    camera.position.z = 15
 
